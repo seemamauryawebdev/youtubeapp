2a0b47e3aaa109d4f2a20c7e0af0ce9baa657b84
seemamauryawebdev <seemamauryadev@gmail.com>
2023-07-23 00:48:02 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import React from 'react'
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      
-    </div>
+    <div>App</div>
   )
 }
-
 export default App
-
