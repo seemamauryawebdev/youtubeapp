b4dcc23491a0a3739835a1ceea2adce5aa170555
seemamauryawebdev <seemamauryadev@gmail.com>
2023-07-25 22:26:16 +0530

    app

---
+++ src/Component/Videodetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Videodetail() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Videodetail
---
+++ src/Component/channelDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function channelDetail() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default channelDetail
