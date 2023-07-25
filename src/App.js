830052a1874d10a1cf56172b1b0d43633933b636
seemamauryawebdev <seemamauryadev@gmail.com>
2023-07-25 22:41:03 +0530

    appp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from '@mui/material';
 
-import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
+import { Navbar, Feed, ChannelDetail, SearchFeed, VideoDetail } from './components';
 
 const App = () => (
   <BrowserRouter>
     <Box sx={{ backgroundColor: '#000' }}>
-      <Navbar />
+      <Navbar/>
       <Routes>
         <Route path="/" exact element={<Feed />} />
         <Route path="/video/:id" element={<VideoDetail />} />
