9070d4e5d11d1bc67b48b1d67730bee263d83a46
seemamauryawebdev <seemamauryadev@gmail.com>
2023-07-27 22:26:54 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from '@mui/material';
 
-import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
+import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './Component';
 
 const App = () => (
   <BrowserRouter>
