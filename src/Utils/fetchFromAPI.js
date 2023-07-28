d260e53dcde35d8ad9c2b674ea9aab049141c897
seemamauryawebdev <seemamauryadev@gmail.com>
2023-07-28 19:08:21 +0530

    app

--- src/Utils/fetchFromAPI.js
+++ src/Utils/fetchFromAPI.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
@@ -6,12 +8,12 @@ const options = {
   },
   headers: {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.co',
   },
 };
 
 export const fetchFromAPI = async (url) => {
-  const { data } = await "axios".get(`${BASE_URL}/${url}`, options);
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
 
   return data;
 };
