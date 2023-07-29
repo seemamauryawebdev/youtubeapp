50327d8a5bc0a4a0398d8e9ee9c0dc4f8604c320
seemamauryawebdev <seemamauryadev@gmail.com>
2023-07-29 18:09:35 +0530

    app

--- src/Utils/fetchFromAPI.js
+++ src/Utils/fetchFromAPI.js
@@ -1,14 +1,15 @@
-import axios from 'axios'
+import axios from 'axios';
 
-export const BASE_URL = 'https://youtube-v311.p.rapidapi.com/activities/'
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
 const options = {
   params: {
-    maxResults: '50'
+    maxResults: 50,
   },
   headers: {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'youtube-v311.p.rapidapi.com'
-  }
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
 };
 
 export const fetchFromAPI = async (url) => {
