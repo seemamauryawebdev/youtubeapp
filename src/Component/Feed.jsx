f4813fbda6f197ce91bc5fa00c5db8f517d2e9e2
seemamauryawebdev <seemamauryadev@gmail.com>
2023-07-29 17:42:50 +0530

    app

--- src/Component/Feed.jsx
+++ src/Component/Feed.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 
-import { fetchFromAPI } from "../Utils/fetchFromAPI";
+import { fetchFromAPI } from "../utils/fetchFromAPI/";
 import { Videos, Sidebar } from "../Component";
 
 const Feed = () => {
@@ -14,7 +14,7 @@ const Feed = () => {
     fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
       .then((data) => setVideos(data.items))
     }, [selectedCategory]);
-
+    
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
--- src/Utils/fetchFromAPI.js
+++ src/Utils/fetchFromAPI.js
@@ -1,15 +1,14 @@
 import axios from 'axios'
 
-export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
-
+export const BASE_URL = 'https://youtube-v311.p.rapidapi.com/activities/'
 const options = {
   params: {
-    maxResults: 50,
+    maxResults: '50'
   },
   headers: {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.co',
-  },
+    'X-RapidAPI-Host': 'youtube-v311.p.rapidapi.com'
+  }
 };
 
 export const fetchFromAPI = async (url) => {
